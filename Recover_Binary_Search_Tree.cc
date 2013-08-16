ae96f3ff293f7e7004c01c64f8500d11a06e0688
iphkwan <iphkwan@qq.com>
2013-08-16 22:32:47 +0800

    Add O(1) space inorder traversal solution.

--- Recover_Binary_Search_Tree.cc
+++ Recover_Binary_Search_Tree.cc
@@ -35,3 +35,55 @@ public:
         }
     }
 };
+
+//O(1) space solution
+//Based on Morris Traversal algorithm (Construct Threads Binary Tree)
+class Solution {
+public:
+    void recoverTree(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (!root)  return;
+        TreeNode *pre = root;
+        TreeNode *cur = root;
+        TreeNode *tmp = NULL;
+        TreeNode *One = NULL;
+        TreeNode *Two = NULL;
+        bool found = false;
+        while (pre->left)   pre = pre->left;
+        while (cur) {
+            if (!cur->left) {
+                if (cur->val < pre->val) {
+                    if (!found) {
+                        found = true;
+                        One = pre;
+                    }
+                    Two = cur;
+                }
+                pre = cur;
+                cur = cur->right;
+            } else {
+                tmp = cur->left;
+                while (tmp->right && tmp->right != cur) {
+                    tmp = tmp->right;
+                }
+                if (!tmp->right) {
+                    tmp->right = cur;
+                    cur = cur->left;
+                } else {
+                    tmp->right = NULL;
+                    if (cur->val < pre->val) {
+                        if (!found) {
+                            found = true;
+                            One = pre;
+                        }
+                        Two = cur;
+                    }
+                    pre = cur;
+                    cur = cur->right;
+                }
+            }
+        }
+        if (found)  swap(One->val, Two->val);
+    }
+};
