f9cade74c5907b9023c488c10ff682217c4c1c69
iphkwan <yexjun@gmail.com>
2014-03-07 17:40:57 +0800

    Create Reverse_Words_in_a_String.cc

---
+++ Reverse_Words_in_a_String.cc
@@ -0,0 +1,42 @@
+//O(1) space solution
+class Solution {
+public:
+    void reverseWords(string &s) {
+        preHandle(s);
+        int left = 0, right;
+        for (right = 0; right < s.length(); right++) {
+            if (s[right] != ' ')
+                continue;
+            if (left < right - 1)
+                reverse(s, left, right - 1);
+            left = right + 1;
+        }
+        if (left < right - 1)
+            reverse(s, left, right - 1);
+        reverse(s, 0, right - 1);
+        return;
+    }
+private:
+    void preHandle(string &s) {
+        int slow = 0, fast = 0;
+        while (slow < s.length()) {
+            while (fast < s.length() && s[fast] == ' ')
+                fast++;
+            if (fast == s.length())
+                break;
+            if (slow != 0 && fast > 0 && s[fast - 1] == ' ')
+                s[slow++] = ' ';
+            s[slow++] = s[fast++];
+        }
+        s.resize(slow);
+    }
+    void reverse(string &s, int st, int ed) {
+        char ch;
+        while (st < ed) {
+            ch = s[st];
+            s[st] = s[ed];
+            s[ed] = ch;
+            st++, ed--;
+        }
+    }
+};
